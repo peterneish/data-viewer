27a12cdf6db63f29d394aed54d732c7f9e0b57cd
Peter Neish <peter.neish@gmail.com>
2015-06-17 17:26:03 +1000

    broken - trying to get routes working with fetch data

--- app.js
+++ app.js
@@ -1,114 +1,103 @@
-var sampleData = [
-  {
-    id: 1,
-    title: 'War and Peace',
-    description: 'The epic tale of love, war and history',
-    Author: 'Tolstoy',
-    price: 7.99
-  },
-  {
-    id: 2,
-    title: 'Anna Karenina',
-    description: 'How things go wrong in love and ultimately lead to suicide. This is why you should not have affairs, girls!',
-    Author: 'Tolstoy',
-    price: 8.50
-  },
-  {
-    id: 3,
-    title: "Fathers and Sons",
-    description: "Another 19th century Russian novel",
-    Author: "Turgenev",
-    price: 11
-  }
-];
+// set up the app
+var app_router;
+var dataset;
+var havedata = false;
 
-//create the new dataset
-//var dataset = new recline.Model.Dataset({
-//  records: sampleData,
-//});
-
-
-var dataset = new recline.Model.Dataset({
-  url: 'https://docs.google.com/spreadsheets/d/1B6LKp6QzNqZFqRXoZ63lgK_T3pluSwtgCN7B1HSV0HU/edit#gid=0',
-  backend: 'gdocs'
-
-})
-
-// Now do the query to the backend to load data
-dataset.fetch().done(function(dataset) {
-  if (console) {
-    //console.log(dataset.records);
-  }
-});
-
-
+fetchData();
 
 // set up the routes
 var AppRouter = Backbone.Router.extend({
     routes: {
         "items/:id": "getItem",
         "search/:qy": "searchItems",
-        "*actions": "defaultRoute"
+        "*actions": "defaultRoute" 
     }
 });
 
-var app_router = new AppRouter;
+app_router = new AppRouter;
 app_router.on('route:getItem', function(id) {
     showItem(id);
-    console.log("item is" + id);
 });
 
 app_router.on('route:searchItems', function(qy){
-  dataset.fetch();
-  dataset.query({"q": qy});
+  doSearch(qy);
 });
+
+app_router.on('route:defaultRoute', function(){
+  console.log("defaultroute");
+})
 
 Backbone.history.start();
 
+// create the search box and add to the page
+var queryEditor = new recline.View.QueryEditor({
+    model: dataset.queryState
+});
+    
+$('#search').append(queryEditor.el); 
+
+function fetchData(callback){
+  
+  if(dataset && typeof dataset.queryState !== 'undefined'){
+     if(callback && typeof callback === 'function' ){
+        callback(dataset);
+      } 
+  }
+  else{
+      dataset = new recline.Model.Dataset({
+        url: 'https://docs.google.com/spreadsheets/d/1B6LKp6QzNqZFqRXoZ63lgK_T3pluSwtgCN7B1HSV0HU/edit#gid=0',
+        backend: 'gdocs'
+      });
+
+      dataset.fetch().done(
+          function(ds){
+            if(callback && typeof callback === 'function' ){
+                callback(dataset);
+            }
+          }
+      );
+
+      // when a record gets queried the records are reset, so this will fire
+      dataset.records.bind('reset', doSearch);
+  
+  }
+
+}
 
 // function that updates the data displayed
-function updateDisplay(){
+function doSearch(qy){
 
-  var content = "";
+  fetchData(function(d){
+      d.query({"q": qy});
 
-  console.log(dataset.records);
+  });
+
+  app_router.navigate("search/" + qy);
+
+
+
 
   dataset.records.each(function(rec){
           content += "<p>title: " + rec.get('title') + " <a href='#/items/" + rec.id + "'>click</a></p>";
   });
 
   $("#data-display").html(content);
-
+  $('#data-display').show();
+  $('#item-display').hide();
 }
 
-function doSearch(){
-  dataset.fetch().done(function(dataset){
 
-    updateDisplay();
-  });
-  updateDisplay();
-  console.log(dataset.queryState.get('q'));
-  app_router.navigate("search/" + dataset.queryState.get('q'));
-
-}
-
+// TODO - use templates or custom view
 function showItem(id){
-  $("#data-display").html(
-      dataset.records.get(id).get('title')
-  )
+
+    fetchData();
+
+    rec = dataset.records.get(id);
+
+      $('#item-display').show();
+      $('#data-display').hide();
+      $('#item-display').html(
+          '<b>'+ rec.get('title') + '</b> '
+               + rec.get('description')
+      );
 }
-
-
-// when a record gets queried the records are reset, so this will fire
-dataset.records.bind('reset', doSearch);
-
-// create the search box and add to the page
-var queryEditor = new recline.View.QueryEditor({
-    model: dataset.queryState
-	  });
-	  
-$('#search').append(queryEditor.el);
-
-
-
-	
